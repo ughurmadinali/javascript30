0cff0c2e4c9c082a1e5e137ddb86ab343d3b289b
ughurmadinali <120271577+ughurmadinali@users.noreply.github.com>
2023-04-20 20:03:20 +0400

    Add files via upload

---
+++ Задание2.js
@@ -0,0 +1,23 @@
+1
+//?
+
+2
+//?
+
+3
+//?
+
+4
+function toArray(obj) {
+    let arr = new Array()
+    for (let x in obj) {
+        arr.push([`${x}`, obj[x]])
+    }
+    return arr
+}
+
+5
+//?
+
+6
+//?
